0e8e2cd359f09ca89facba2a6c9366739f4cb7ee
Alvin <sanghup123@naver.com>
2020-02-26 17:43:39 +0900

    Updates

---
+++ precache-manifest.a16a8d31f0d4350c766219d3d426000e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7a267596a9aa935d11989ca406d02b",
+    "url": "/front-task/index.html"
+  },
+  {
+    "revision": "ac118c279282ac79c8f2",
+    "url": "/front-task/static/css/main.e38c1227.chunk.css"
+  },
+  {
+    "revision": "3a66f8b52d5b2bf2fcda",
+    "url": "/front-task/static/js/2.e936665d.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/front-task/static/js/2.e936665d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65752565d306d653d891",
+    "url": "/front-task/static/js/3.57eab181.chunk.js"
+  },
+  {
+    "revision": "0b1763024b52bf63b841",
+    "url": "/front-task/static/js/4.d6de9c20.chunk.js"
+  },
+  {
+    "revision": "ac118c279282ac79c8f2",
+    "url": "/front-task/static/js/main.f1e0eeb8.chunk.js"
+  },
+  {
+    "revision": "7fe30a2e665e6d4b4e33",
+    "url": "/front-task/static/js/runtime-main.4054d682.js"
+  }
+]);
