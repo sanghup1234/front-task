54bdbc9237c2761eb87029e4cecd63c5c9fdd784
Alvin <sanghup123@naver.com>
2020-02-26 17:38:59 +0900

    Fit: ⚡️ Add Root component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Root from "./Root";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -11,7 +11,7 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Root />
   </Provider>,
   document.getElementById("root")
 );
